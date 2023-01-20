bccecd4e5755857657c11ed6cbb88b2e3c7ee0dd
Muhammad Umer Saeed <122075777+MuhammadUmerSaeed98@users.noreply.github.com>
2023-01-19 22:44:22 -0800

    ch1

---
+++ src/Even.java
@@ -0,0 +1,32 @@
+public class Main {
+	public static void main(String[] args) {
+		Main c = new Main();
+		int number = 3;
+		System.out.println(c.evenInteger(number));
+		System.out.println(c.isPrime(number));
+		
+		
+	}
+	public static String evenInteger(int number) {
+		if(number % 2 == 0) {
+			return "Even Number";
+		}
+		return "Not Even Number";
+	}
+	
+	 public static boolean isPrime(int n)
+	    {
+	        if (n <= 1) {
+	            return false;
+	        }
+	        for (int i = 2; i < n; i++) {
+	            if (n % i == 0) {
+	                return false;
+	            }
+	        }
+	  
+	        return true;
+	    }
+}
+
+
---
+++ src/Test4.java
@@ -0,0 +1,19 @@
+
+public class Test4 {
+       public static void main(String args[]) {
+	   try
+	   {
+		   int a=100,b=0,c;
+		   c=a/b;
+		   System.out.println(c);
+		   System.out.println("Software Design and Architecture");
+		   System.out.println("Computer Science");}
+		   catch(Exception e) {
+			   System.out.println(e); }
+			   System.out.println("Information Security");
+		   }   System.out.println("Software Construction and Development");
+		   
+	   }
+	
+
+
